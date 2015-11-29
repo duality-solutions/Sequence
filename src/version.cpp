bd3a8c63712ef267f3c12b5a476064831ba7033f
SCDeveloper <silkcoindeveloper@gmail.com>
2015-11-29 01:45:13 +0000

    Versioning Fixes

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Silkoshi");
+const std::string CLIENT_NAME("SLK");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   ""
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("Silkoshi");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "$Format:%cD"
+#    define GIT_COMMIT_DATE ""
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
