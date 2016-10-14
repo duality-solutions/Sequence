40c93bd20f3a1474e8e3870ec4e9db96e6bca05d
GregoryBetz <g.betz@silknetwork.org>
2016-10-14 03:49:26 -0400

    [C++11] Removes scoped enums declaration (now redundant)

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -14,10 +14,7 @@
 #include "utiltime.h"
 #include "wallet.h"
 
-#define BOOST_NO_CXX11_SCOPED_ENUMS
 #include <boost/filesystem.hpp>
-#undef BOOST_NO_CXX11_SCOPED_ENUMS
-
 #include <boost/foreach.hpp>
 #include <boost/scoped_ptr.hpp>
 #include <boost/thread.hpp>
