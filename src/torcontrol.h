005d625b737612cb2ce093c27184feb11ca81740
Ahmad Kazi <empinelx@gmail.com>
2016-10-22 06:16:22 -0700

    [Tor] Add stubs files pertaining to The Onion Router Implementations

---
+++ src/torcontrol.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2015 The BitCoin Core developers
+// Copyright (c) 2016 The Silk Network developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+/**
+ * Functionality for communicating with Tor.
+ */
+#ifndef SILK_TORCONTROL_H
+#define SILK_TORCONTROL_H
+
+#include "scheduler.h"
+
+extern const std::string DEFAULT_TOR_CONTROL;
+static const bool DEFAULT_LISTEN_ONION = true;
+
+void StartTorControl(boost::thread_group& threadGroup, CScheduler& scheduler);
+void InterruptTorControl();
+void StopTorControl();
+
+#endif /* SILK_TORCONTROL_H */
