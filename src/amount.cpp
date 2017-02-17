d66dae474541ed086a9823107204421786bb22f2
Spencer Lievens <spencer.lievens@sterlingcoin.org.uk>
2017-02-17 17:56:00 +0100

    amounts

--- src/amount.cpp
+++ src/amount.cpp
@@ -30,7 +30,7 @@ CAmount CFeeRate::GetFee(size_t nSize) const
 
 std::string CFeeRate::ToString() const
 {
-    return strprintf("%d.%08d %s/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN, CURRENCY_UNIT);
+    return strprintf("%d.%06d %s/kB", nSatoshisPerK / COIN, nSatoshisPerK % COIN, CURRENCY_UNIT);
 }
 
 CAmount GetMinFee(size_t nBytes, size_t nBlockSize)
