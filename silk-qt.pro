f51bf35556b45e53721bcd2177a02ccab6ef1d28
coin-info-net <coin-info.net@t-online.de>
2017-02-25 15:08:08 +0100

    Gitian Fix #1

--- silk-qt.pro
+++ silk-qt.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 TARGET = Silk
-VERSION = 1.2.0.0
+VERSION = 1.3.0.0
 
 # for boost 1.37, add -mt to the boost libraries
 # use: qmake BOOST_LIB_SUFFIX=-mt
--- src/qt/silk.cpp
+++ src/qt/silk.cpp
@@ -8,9 +8,6 @@
 #include "config/silk-config.h"
 #endif
 
-#include "silkgui.h"
-
-#include "chainparams.h"
 #include "clientmodel.h"
 #include "guiconstants.h"
 #include "guiutil.h"
@@ -18,20 +15,22 @@
 #include "net.h"
 #include "networkstyle.h"
 #include "optionsmodel.h"
+#include "silkgui.h"
 #include "splashscreen.h"
 #include "utilitydialog.h"
 #include "winshutdownmonitor.h"
 
-#ifdef ENABLE_WALLET
-#include "paymentserver.h"
-#include "walletmodel.h"
-#endif
-
+#include "chainparams.h"
 #include "init.h"
 #include "rpc/rpcserver.h"
 #include "scheduler.h"
 #include "ui_interface.h"
 #include "util.h"
+
+#ifdef ENABLE_WALLET
+#include "paymentserver.h"
+#include "walletmodel.h"
+#endif
 
 #ifdef ENABLE_WALLET
 #include "wallet/wallet.h"
@@ -63,7 +62,9 @@ Q_IMPORT_PLUGIN(qtwcodecs)
 Q_IMPORT_PLUGIN(qkrcodecs)
 Q_IMPORT_PLUGIN(qtaccessiblewidgets)
 #else
+#if QT_VERSION < 0x050400
 Q_IMPORT_PLUGIN(AccessibleFactory)
+#endif
 #if defined(QT_QPA_PLATFORM_XCB)
 Q_IMPORT_PLUGIN(QXcbIntegrationPlugin);
 #elif defined(QT_QPA_PLATFORM_WINDOWS)
