51e42f07b6c7f60598ba19f65b6243c6e134fe05
Spencer Lievens <36229929+Duality-CDOO@users.noreply.github.com>
2018-03-29 11:38:12 +0200

    Fix decimal amount Pt.2

--- src/utilmoneystr.cpp
+++ src/utilmoneystr.cpp
@@ -19,7 +19,7 @@ string FormatMoney(const CAmount& n, bool fPlus)
     int64_t n_abs = (n > 0 ? n : -n);
     int64_t quotient = n_abs/COIN;
     int64_t remainder = n_abs%COIN;
-    string str = strprintf("%d.%06d", quotient, remainder);
+    string str = strprintf("%d.%08d", quotient, remainder);
 
     // Right-trim excess zeros before the decimal point:
     int nTrim = 0;
