cf31bbb922b82a3a1e513053ecb09127dcd0ef3a
Duality-CDOO <s.lievens@duality.solutions>
2019-01-28 12:45:02 +0100

    [Fix] Spent Stake Attack

--- src/main.cpp
+++ src/main.cpp
@@ -54,6 +54,15 @@ using namespace std;
 CCriticalSection cs_main;
 
 BlockMap mapBlockIndex;
+
+// maps any spent outputs in the past 101 blocks to the height it was spent
+// this means for incoming blocks, we can check that their stake output was not spent before
+// the incoming block tried to use it as a staking input. We can also prevent block spam
+// attacks because then we can check that either the staking input is available in the current
+// active chain, or the staking input was spent in the past 100 blocks after the height
+// of the incoming block.
+map<COutPoint, int> mapStakeSpent;
+
 CChain chainActive;
 CBlockIndex *pindexBestHeader = NULL;
 int64_t nTimeBestReceived = 0;
@@ -1722,6 +1731,9 @@ bool DisconnectBlock(CBlock& block, CValidationState& state, CBlockIndex* pindex
                 if (coins->vout.size() < out.n+1)
                     coins->vout.resize(out.n+1);
                 coins->vout[out.n] = undo.txout;
+
+                // erase the spent input
+                mapStakeSpent.erase(out);
             }
         }
     }
@@ -2045,7 +2057,23 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
         if (!pblocktree->WriteTxIndex(vPos))
             return state.Abort("Failed to write transaction index");
 
-    // add this block to the view's block chain
+    // add new entries
+    for (const CTransaction tx: block.vtx) {
+        if (tx.IsCoinBase())
+            continue;
+        for (const CTxIn in: tx.vin) {
+            mapStakeSpent.insert(std::make_pair(in.prevout, pindex->nHeight));
+        }
+    }
+
+     // delete old entries
+    for (auto it = mapStakeSpent.begin(); it != mapStakeSpent.end(); ++it) {
+        if (it->second < pindex->nHeight - 101) {
+            mapStakeSpent.erase(it->first);
+        }
+    }
+
+    // add this block to the view's blockchain
     view.SetBestBlock(pindex->GetBlockHash());
 
     int64_t nTime3 = GetTimeMicros(); nTimeIndex += nTime3 - nTime2;
@@ -3108,6 +3136,8 @@ bool AcceptBlock(CBlock& block, CValidationState& state, const CChainParams& cha
 {
     AssertLockHeld(cs_main);
 
+    CBlockIndex* pindexPrev = NULL;
+
     CBlockIndex *&pindex = *ppindex;
 
     if (!AcceptBlockHeader(block, block.IsProofOfStake(), state, chainparams, &pindex))
@@ -3132,6 +3162,60 @@ bool AcceptBlock(CBlock& block, CValidationState& state, const CChainParams& cha
         return error("AcceptBlock() : rejected by synchronized checkpoint");
 
     int nHeight = pindex->nHeight;
+
+    if (block.IsProofOfStake()) {
+        LOCK(cs_main);
+
+         CCoinsViewCache coins(pcoinsTip);
+
+         if (!coins.HaveInputs(block.vtx[1])) {
+
+            // the inputs are spent at the chain tip so we should look at the recently spent outputs
+            for (CTxIn in : block.vtx[1].vin) {
+                auto it = mapStakeSpent.find(in.prevout);
+                if (it == mapStakeSpent.end()) {
+                    return false;
+                }
+                if (it->second <= pindexPrev->nHeight) {
+                    return false;
+                }
+            }
+        }
+
+        // if this is on a fork
+        if (!chainActive.Contains(pindexPrev) && pindexPrev != NULL) {
+
+            // start at the block we're adding on to
+            CBlockIndex *last = pindexPrev;
+
+            // while that block is not on the main chain
+            while (!chainActive.Contains(last) && pindexPrev != NULL) {
+                CBlock bl;
+                ReadBlockFromDisk(bl, last);
+
+                // loop through every spent input from said block
+                for (CTransaction t : bl.vtx) {
+                    for (CTxIn in: t.vin) {
+
+                        // loop through every spent input in the staking transaction of the new block
+                        for (CTxIn stakeIn : block.vtx[1].vin) {
+
+                            // if they spend the same input
+                            if (stakeIn.prevout == in.prevout) {
+
+                                // reject the block
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                // go to the parent block
+                last = pindexPrev->pprev;
+            }
+        }
+    }
+
     const Consensus::Params& consensusParams = Params().GetConsensus();
     if (block.IsProofOfWork() && nHeight > consensusParams.nLastPOWBlock)
         return state.DoS(100, error("AcceptBlock() : reject proof-of-work at height %d", nHeight));
